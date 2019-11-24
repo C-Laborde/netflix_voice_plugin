63a86fe446be688e29f1f108170dd512e92a075f
cecilia_laborde <cecilia@intellisense.io>
2019-11-24 17:59:41 +0100

    rm unnecessary files

--- auto-trigger.js
+++ auto-trigger.js
@@ -1,9 +1,24 @@
+// --enable-blink-features=GetUserMedia
 setTimeout(() => {
-    navigator.mediaDevices.getUserMedia({ audio: true })
+  navigator.permissions.query({name:'microphone'}).then(function(result) {
+      if (result.state == 'granted') {
+          console.log("Granted")
+      } else if (result.state == 'prompt') {
+          console.log("Prompt")
+      } else if (result.state == 'denied') {
+          console.log("Denied")
+      }
+      });
+
+    navigator.mediaDevices.getUserMedia({audio: true})
+    .then(function(stream){
+        console.log("Streaming!")
+    })
     .catch(function() {
-        chrome.tabs.create({
-            url: chrome.extension.getURL("options.html"),
-            selected: true
-        })
+        // chrome.tabs.create({
+        //     url: chrome.extension.getURL("options.html"),
+        //     selected: true
+        // })
+        console.log("GetUserMedia not working")
     });
-}, 100);
+}, 1000);
